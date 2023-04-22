2e92dccd778bf40f377bd05bf1e0ac20576fffab
eunsookim1 <92eunsoo@gmail.com>
2023-04-23 01:09:14 +0900

    final changes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,25 @@
 const request = require('request');
 
-const name = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${name}`;
+const fetchBreedDescription = function(breedName, callback) {
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("breed is not found");
-    return;
-  }
-  console.log(data[0].description);
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   
-});
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("breed is not found", null);
+      return;
+    }
+
+    callback(null, data[0].description);
+  
+  });
+};
+
+module.exports = {fetchBreedDescription};
 // command line app that retrieves description of a specific breed if found in the list of cat breeds.
